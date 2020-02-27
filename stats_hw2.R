fb087d43c54550b38e939a6f7d29a9c29b0c13eb
Aaron Goyzueta <aarongoyzueta@gmail.com>
2020-02-26 19:23:33 -0500

    Added preliminary answers and R commands w/out stretch goals

--- stats_hw2.R
+++ stats_hw2.R
@@ -1,1 +1,19 @@
 # R commands go here
+
+# Part 1
+x <- 501
+n <- 501 + 1859
+
+binom.test(x, n, .5)
+
+# Part 2
+df <- read.table("PTB.tsv", sep="\t", header=TRUE)
+
+df_stan <- df[df$Stanford.tag==df$gold.tag, ]
+df_nlp <- df[df$NLP4J.tag==df$gold.tag, ]
+
+x1 <- nrow(df_stan[df_stan$gold.tag!=df_stan$NLP4J.tag, ])
+x2 <- nrow(df_nlp[df_nlp$gold.tag!=df_nlp$Stanford.tag, ])
+x <- min(x1, x2)
+n <- x1 + x2
+binom.test(x, n, .5)
